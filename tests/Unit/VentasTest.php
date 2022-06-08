b611c574dba639b087da3807d0eb468ee05f4ce8
David03T <davftorres@misena.edu.co>
2022-06-08 11:54:03 -0500

    hecho

---
+++ tests/Unit/VentasTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class VentasTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_ventas()
+    {
+        $this->assertTrue(true);
+    }
+}
