fbf81a6a8d262c801d842cd76cabcd97e5372080
David03T <davftorres@misena.edu.co>
2022-06-23 07:48:22 -0500

    productos

--- app/Http/Controllers/productosController.php
+++ app/Http/Controllers/productosController.php
@@ -9,7 +9,7 @@ class productosController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except("index");
     }
     /**
      * Display a listing of the resource.
